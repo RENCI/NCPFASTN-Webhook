1d0864e65906bb7a479bf477ddd82169f8acba2c
Tripp Tuttle <tripp.tuttle@unc.edu>
2019-07-02 14:56:16 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 from flask import Flask, request, Response
 import os.path
+import threading
+
 
 application = Flask(__name__)
 
 token = os.getenv('WEBHOOK_TOKEN', "default")
 
+cmdList = {"name": "Command Details"}
+
 
 @application.route('/webhook', methods=['POST', 'GET'])
 def webhook():
-    if request.method == 'POST':
-        if request.get_json(force=True):
-            payload = request.get_json(force=True)
-            if payload.get("token"):
-                if payload['token'] == token:
-                    f = open('_requests.txt', 'w')
-                    f.write(payload['app'])
-                    f.close()
-                    return Response('Accepted', 202)
-                else:
-                    return Response('Unauthorized Authentication Token', 401)
-            else:
-                return Response('No Authentication Token Sent', 401)
+    for i in request.args.post():
+        print(i)
+    if request.args.get('token', '') == token:
+        if request.args.get('cmd', '') in cmdList:
+            try:
+                cmd = request.args.get('cmd')
+                f = open('_requests.txt', 'w')
+                f.write(cmd)
+                f.close()
+                th = threading.Thread(target=runCommand(cmd))
+                th.start()
+                return Response('Accepted', 202)
+            except OSError:
+                return Response('Server Error', 500)
         else:
-            return Response('Incorrect Request Syntax', 400)
+            return Response('Invalid Command', 418)
     else:
-        return Response('Invalid Method', 405)
+        return Response('Unauthorized Authentication Token', 401)
 
+# else:
+# return Response('Invalid HTTP Method', 405)
+
+
+def runCommand(cmd):
+    print("trying to do this command name: " + cmd)
