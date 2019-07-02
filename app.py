06f4b2e40858a94c61cd0e259f138831245b333c
Tripp Tuttle <tripp.tuttle@unc.edu>
2019-07-02 17:25:30 -0400

    basics working, need to define logic for choosing which command and image to act on.

--- app.py
+++ app.py
@@ -1,39 +1,49 @@
 from flask import Flask, request, Response
 import os.path
 import threading
-
+import hmac
+import docker
 
 application = Flask(__name__)
 
-token = os.getenv('WEBHOOK_TOKEN', "default")
+secretToken = os.getenv('WEBHOOK_TOKEN', "default")
 
-cmdList = {"name": "Command Details"}
+cmdList = {"rebuild": "Command Details"}
 
 
 @application.route('/webhook', methods=['POST', 'GET'])
 def webhook():
-    for i in request.args.post():
-        print(i)
-    if request.args.get('token', '') == token:
-        if request.args.get('cmd', '') in cmdList:
-            try:
-                cmd = request.args.get('cmd')
-                f = open('_requests.txt', 'w')
-                f.write(cmd)
-                f.close()
-                th = threading.Thread(target=runCommand(cmd))
-                th.start()
-                return Response('Accepted', 202)
-            except OSError:
-                return Response('Server Error', 500)
+    if request.method == 'POST':
+        if request.headers('HTTP_X_HUB_SIGNATURE'):
+            receivedToken = request.headers('HTTP_X_HUB_SIGNATURE')
+            if validateSecretToken(receivedToken):
+                if request.args.get('cmd', '') in cmdList:
+                    try:
+                        cmd = request.args.get('cmd')
+                        f = open('_requests.txt', 'w')
+                        f.write(cmd)
+                        f.close()
+                        th = threading.Thread(target=runCommand(cmd))
+                        th.start()
+                        return Response('Accepted', 202)
+                    except OSError:
+                        return Response('Server Error', 500)
+                else:
+                    return Response('Invalid Command', 418)
+            else:
+                return Response('Unauthorized Authentication Token', 401)
         else:
-            return Response('Invalid Command', 418)
+            return Response('No Secret Token Provided in Header', 401)
     else:
-        return Response('Unauthorized Authentication Token', 401)
+        return Response('Invalid Method', 405)
 
-# else:
-# return Response('Invalid HTTP Method', 405)
+
+def validateSecretToken(receivedToken):
+    hashReceived = hmac.new(receivedToken).hexdigest()
+    hashSecret = hmac.new(secretToken).hexdigest()
+    return hmac.compare_digest(hashReceived, hashSecret)
 
 
 def runCommand(cmd):
-    print("trying to do this command name: " + cmd)
+    d = docker.from_env()
+    d.build(path='/srv/datahub/webapp')
