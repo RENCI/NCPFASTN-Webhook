3755838f934157d40634fdffabe21fce97309dd4
Tripp Tuttle <tripp.tuttle@unc.edu>
2019-07-03 12:07:17 -0400

    basics working, need to define logic for choosing which command and image to act on.

--- app.py
+++ app.py
@@ -1,49 +1,61 @@
 from flask import Flask, request, Response
 import os.path
+import subprocess
 import threading
 import hmac
 import docker
+import pprint as pp
+
 
 application = Flask(__name__)
+
 application.config['DEBUG'] = True
 
 secretToken = os.getenv('WEBHOOK_TOKEN', "default")
 
-cmdList = {"rebuild": "Command Details"}
+cmdList = {"rebuild": "docker-compose build --no-cache && docker-compose up -d"}
 
 
 @application.route('/webhook', methods=['POST', 'GET'])
 def webhook():
-    if request.method == 'POST':
-        if request.headers('HTTP_X_HUB_SIGNATURE'):
-            receivedToken = request.headers('HTTP_X_HUB_SIGNATURE')
-            if validateSecretToken(receivedToken):
-                if request.args.get('cmd') in cmdList:
-                    try:
-                        # cmd = request.args.get('cmd')
-                        # f = open('_requests.txt', 'w')
-                        # f.write(cmd)
-                        # f.close()
-                        # th = threading.Thread(target=runCommand(cmd))
-                        # th.start()
-                        return Response('Accepted', 202)
-                    except OSError:
-                        return Response('Server Error', 500)
-                else:
-                    return Response('Invalid Command', 418)
-            else:
-                return Response('Unauthorized Authentication Token', 401)
-        else:
-            return Response('No Secret Token Provided in Header', 401)
-    else:
+    if request.method != 'POST':
         return Response('Invalid Method', 405)
 
+    if not request.headers.get('X-Hub-Signature'):
+        return Response('No Secret Token Provided in Header', 401)
 
-def validateSecretToken(receivedToken):
-    hashSecret = 'sha1=' + hmac.new(secretToken).hexdigest()
-    return hmac.compare_digest(hashSecret, receivedToken)
+    sha, signature = request.headers.get('X-Hub-Signature').split('=')
+    auth, hash = validateSecretToken(signature, request.data, secretToken)
+    if not auth:
+        return Response('Unauthorized Authentication Token', 401)
+
+    if not request.args.get('cmd'):
+        return Response('No Command Provided! \nValid Commands: ' + pp.pprint(cmdList), 518)
+
+    cmd = request.args.get('cmd')
+    if cmd not in cmdList:
+        return Response('Command ' + str(cmd) + ' Not Configured! \n Valid Commands:' + pp.pprint(cmdList), 518)
+    try:
+        # f = open('_requests.txt', 'w')
+        # f.write(cmd)
+        # f.close()
+        th = threading.Thread(target=runCommand, args=(cmd,), daemon=True)
+        th.daemon = True
+        th.start()
+        return Response('Accepted, starting command', 202)
+    except IOError as err:
+        return Response('Server Error: ' + err.strerror, 500)
+
+
+def validateSecretToken(sig, data, secretToken):
+    sig = bytearray(sig, 'utf-8')
+    secretToken = bytearray(secretToken, 'utf-8')
+    sigSecret = hmac.new(secretToken, msg=data, digestmod='sha1')
+    digest = bytearray(sigSecret.hexdigest(), 'utf-8')
+    print(digest)
+    return hmac.compare_digest(digest, sig), digest
 
 
 def runCommand(cmd):
-    d = docker.from_env()
-    d.build(path='/srv/datahub/webapp')
+    toRun = cmdList[cmd]
+    subprocess.call(toRun, cwd='/srv/datahub', shell=True)
