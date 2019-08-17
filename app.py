ad485175ee8a3780ef1e78689ffeb584e674e91a
Tripp Tuttle <tripp.tuttle@unc.edu>
2019-08-16 20:34:32 -0400

    Adding some logging to push to Graylog

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ import os.path
 import pprint as pp
 import subprocess
 import threading
+import graypy
+import logging
+import uuid
 
 from flask import Flask, request, Response
+
+
+log_setup = logging.getLogger('webhook-listener')
+log_setup.setLevel(logging.DEBUG)
+
+handler = graypy.GELFUDPHandler('192.168.16.6', 12201)
+log_setup.addHandler(handler)
+
 
 
 application = Flask(__name__)
@@ -21,7 +32,8 @@ cmdList = {
             },
     'rebuild-webapp':
         {
-            'script': 'docker-compose build --no-cache django && docker-compose up -d',
+            'script': 'docker-compose build --no-cache django && docker-compose up -d django && docker container '
+                      'restart nginx',
             'dir': '/srv/datahub'
             }
     }
@@ -30,24 +42,31 @@ cmdList = {
 # flask route/server
 @application.route('/webhook', methods=['POST', 'GET'])
 def webhook():
+    myLogger = logging.LoggerAdapter(logging.getLogger('webhook-listener'),
+                                     {'trigger-id': uuid.uuid4()})
     if request.method != 'POST':
+        myLogger.error('Invalid Method on Webhook')
         return Response('Invalid Method', 405)
 
     if not request.headers.get('X-Hub-Signature'):
+        myLogger.error('WebNo Secret Token Provided in Header')
         return Response('No Secret Token Provided in Header', 401)
 
     sha, signature = request.headers.get('X-Hub-Signature').split('=')
     auth, authHash = validateSecretToken(signature, request.data, secretToken)
 
     if not auth:
+        myLogger.error('Webhook: Invalid Token')
         return Response('Unauthorized Authentication Token', 401)
 
     if not request.args.get('cmd'):
+        myLogger.error('Webhook: No Command Provided')
         return Response('No Command Provided! \nValid Commands: ' + pp.pprint(cmdList), 518)
 
     cmd = request.args.get('cmd')
 
     if cmd not in cmdList:
+        myLogger.error('Webhook: Command Was Invalid')
         return Response('Command ' + str(cmd) + ' Not Configured! \n Valid Commands:' + pp.pprint(cmdList), 518)
 
     try:
@@ -55,9 +74,11 @@ def webhook():
         th = threading.Thread(target=runCommand, args=(command,), daemon=True)
         th.daemon = True
         th.start()
+        myLogger.info('Webhook: Accepted, Starting Command Run')
         return Response('Accepted, starting command', 202)
 
     except IOError as err:
+        myLogger.error('Server Error: ' + err.strerror)
         return Response('Server Error: ' + err.strerror, 500)
 
 
